199728ab78bdb19c75c1e6e024b6de87b3e97b44
ziya <koltikin210@gmail.com>
2023-11-16 23:16:28 +0300

    update and delete endpoints are done

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,15 +1,13 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.TaskDTO;
 import com.cydeo.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.filters.AddDefaultCharsetFilter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 
@@ -34,14 +32,42 @@ public class TaskController {
         return ResponseEntity.ok(
                 ResponseWrapper.builder()
                         .success(true)
-                        .message("Tasks are successfully retrieved")
+                        .message("Task is successfully retrieved")
                         .code(HttpStatus.OK.value())
                         .data(taskService.findById(id)).build()
         );
     }
-    public ResponseEntity<ResponseWrapper> createTask(){}
-    public ResponseEntity<ResponseWrapper> deleteTask(){}
-    public ResponseEntity<ResponseWrapper> updateTask(){}
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createTask(@RequestBody TaskDTO taskDTO){
+        taskService.save(taskDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ResponseWrapper.builder()
+                        .success(true)
+                        .message("Task is successfully created")
+                        .code(HttpStatus.OK.value()).build()
+        );
+    }
+    @DeleteMapping("/{taskId}")
+    public ResponseEntity<ResponseWrapper> deleteTask(@PathVariable("taskId") Long id){
+        taskService.delete(id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ResponseWrapper.builder()
+                        .success(true)
+                        .message("Task is successfully deleted")
+                        .code(HttpStatus.CREATED.value()).build()
+        );
+    }
+    @PutMapping
+    public ResponseEntity<ResponseWrapper> updateTask(@RequestBody TaskDTO taskDTO){
+        taskService.update(taskDTO);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(
+                ResponseWrapper.builder()
+                        .success(true)
+                        .message("Task is successfully updated")
+                        .code(HttpStatus.ACCEPTED.value()).build()
+        );
+
+    }
     public ResponseEntity<ResponseWrapper> employeePendingTasks(){}
     public ResponseEntity<ResponseWrapper> employeeUpdateTasks(){}
     public ResponseEntity<ResponseWrapper> employeeArchiveTasks(){}
