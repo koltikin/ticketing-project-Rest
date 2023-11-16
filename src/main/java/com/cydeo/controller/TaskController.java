7108c8da6a5b751b02f90f8f75b6f12cf26fee1a
ziya <koltikin210@gmail.com>
2023-11-16 23:10:12 +0300

    getTasks and getTaskById endpoints are done

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,19 +1,44 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.filters.AddDefaultCharsetFilter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/task")
 public class TaskController {
+    private final TaskService taskService;
 
-    public ResponseEntity<ResponseWrapper> getTasks(){}
-    public ResponseEntity<ResponseWrapper> getTaskById(){}
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getTasks(){
+        return ResponseEntity.ok(
+                ResponseWrapper.builder()
+                        .success(true)
+                        .message("Tasks are successfully retrieved")
+                        .code(HttpStatus.OK.value())
+                        .data(taskService.listAllTasks()).build()
+        );
+    }
+    @GetMapping("/{taskId}")
+    public ResponseEntity<ResponseWrapper> getTaskById(@PathVariable("taskId") Long id){
+        return ResponseEntity.ok(
+                ResponseWrapper.builder()
+                        .success(true)
+                        .message("Tasks are successfully retrieved")
+                        .code(HttpStatus.OK.value())
+                        .data(taskService.findById(id)).build()
+        );
+    }
     public ResponseEntity<ResponseWrapper> createTask(){}
     public ResponseEntity<ResponseWrapper> deleteTask(){}
     public ResponseEntity<ResponseWrapper> updateTask(){}
