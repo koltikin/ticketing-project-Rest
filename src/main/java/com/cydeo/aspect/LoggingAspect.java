7619bd138e96a48fd02662917587a2c59f6f444c
ziya <koltikin210@gmail.com>
2023-11-22 12:48:17 +0300

    dependency added and logger class created

---
+++ src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -0,0 +1,14 @@
+package com.cydeo.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+//    Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+}
